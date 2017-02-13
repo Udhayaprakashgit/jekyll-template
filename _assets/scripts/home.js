a2338f97e635bcac9f7cac8f55894be352934e88
Udhayaprakash <udhayaprakash.t@hashedin.com>
2017-02-13 15:01:25 +0530

    review comment fixes

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,17 +1,14 @@
 //Write JS logic here
 
-  var y = document.getElementById("links");
+  var nav_links = document.getElementById("links");
   var name = 'nav-display';
   var prev= 'navigation-right';
   function menu(){
-console.log("hellllllloooo");
-    if(y.classList.contains(name)){
-      console.log("in if condition",y.classList.contains(name));
-      y.classList.remove(name);
+    if(nav_links.classList.contains(name)){
+      nav_links.classList.remove(name);
     }
     else{
-      console.log("in else condition", y.classList.contains(name));
-      y.classList.add(name);
+      nav_links.classList.add(name);
     }
 
     }
