727477b08ce80b61f54201adf596f63ebb3e6f27
Udhayaprakash <udhayaprakash.t@hashedin.com>
2017-02-13 12:23:16 +0530

    humberger created

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,1 +1,13 @@
 //Write JS logic here
+
+  var x = document.getElementById("nav-button");
+  var y = document.getElementById("links");
+  var name = 'nav-display';
+  var prev= 'navigation-right';
+  x.addEventListener("click", function(){
+    if(y.classList.contains(name)){
+      y.classList.remove(name);
+    }
+    else
+      y.classList.add(name);
+    });
